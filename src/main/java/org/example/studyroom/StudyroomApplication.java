c700ca2659c8dfd50ba1b89f739c6ea5338a55d3
xiqing <gongyaobo_2020@qq.com>
2026-09-11 11:25:04 +0800

    0206.10.11

---
+++ src/main/java/org/example/studyroom/StudyroomApplication.java
@@ -0,0 +1,13 @@
+package org.example.studyroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyroomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyroomApplication.class, args);
+    }
+
+}
